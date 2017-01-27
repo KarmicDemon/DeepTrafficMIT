b12301c60cfc2b097b7783f664f3da06f73b0083
Chukwudi Derek Uche <uchederek@yahoo.com>
2017-01-27 18:37:14 -0500

    Refactor

--- net.js
+++ net.js
@@ -1,7 +1,9 @@
 
 //<![CDATA[
 
-// a few things don't have var in front of them - they update already existing variables the game needs
+// a few things don't have var in front of them -
+// they update already existing variables the game needs
+
 lanesSide = 0;
 patchesAhead = 1;
 patchesBehind = 0;
@@ -10,7 +12,8 @@ trainIterations = 10000;
 var num_inputs = (lanesSide * 2 + 1) * (patchesAhead + patchesBehind);
 var num_actions = 5;
 var temporal_window = 3;
-var network_size = num_inputs * temporal_window + num_actions * temporal_window + num_inputs;
+var network_size = num_inputs * temporal_window + num_actions
+    * temporal_window + num_inputs;
 
 var layer_defs = [];
 layer_defs.push({
@@ -19,11 +22,13 @@ layer_defs.push({
     out_sy: 1,
     out_depth: network_size
 });
+
 layer_defs.push({
     type: 'fc',
     num_neurons: 1,
     activation: 'relu'
 });
+
 layer_defs.push({
     type: 'regression',
     num_neurons: num_actions
@@ -61,8 +66,128 @@ learn = function (state, lastReward) {
 }
 
 //]]>
-    
+
 /*###########*/
 if (brain) {
-brain.value_net.fromJSON({"layers":[{"out_depth":19,"out_sx":1,"out_sy":1,"layer_type":"input"},{"out_depth":1,"out_sx":1,"out_sy":1,"layer_type":"fc","num_inputs":19,"l1_decay_mul":0,"l2_decay_mul":1,"filters":[{"sx":1,"sy":1,"depth":19,"w":{"0":-0.37165577312364945,"1":-0.2653322251249961,"2":-0.015307829790315669,"3":0.16031886623305236,"4":-0.17324644602384698,"5":-0.2313851098218338,"6":0.32800273412543784,"7":-0.02829250208285845,"8":-0.029796504442146224,"9":-0.09624497169753406,"10":-0.14203333942141338,"11":0.13346486244544334,"12":-0.07300195145118349,"13":0.032159527658345524,"14":-0.44566258359836214,"15":-0.05417868981875934,"16":-0.030150343272387907,"17":-0.22363720331452142,"18":0.12532496545456903}}],"biases":{"sx":1,"sy":1,"depth":1,"w":{"0":0.1}}},{"out_depth":1,"out_sx":1,"out_sy":1,"layer_type":"relu"},{"out_depth":5,"out_sx":1,"out_sy":1,"layer_type":"fc","num_inputs":1,"l1_decay_mul":0,"l2_decay_mul":1,"filters":[{"sx":1,"sy":1,"depth":1,"w":{"0":-0.8326883805490437}},{"sx":1,"sy":1,"depth":1,"w":{"0":-1.97764739660907}},{"sx":1,"sy":1,"depth":1,"w":{"0":0.943349904683505}},{"sx":1,"sy":1,"depth":1,"w":{"0":-0.9824216301888558}},{"sx":1,"sy":1,"depth":1,"w":{"0":-1.493710164013261}}],"biases":{"sx":1,"sy":1,"depth":5,"w":{"0":0,"1":0,"2":0,"3":0,"4":0}}},{"out_depth":5,"out_sx":1,"out_sy":1,"layer_type":"regression","num_inputs":5}]});
+    brain.value_net.fromJSON({
+        "layers":[{
+            "out_depth":19,
+            "out_sx":1,
+            "out_sy":1,
+            "layer_type":"input"
+        },
+        {"out_depth":1,
+        "out_sx":1,
+        "out_sy":1,
+        "layer_type":"fc",
+        "num_inputs":19,
+        "l1_decay_mul":0,
+        "l2_decay_mul":1,
+        "filters":[{
+            "sx":1,
+            "sy":1,
+            "depth":19,
+            "w":{
+                "0":-0.37165577312364945,
+                "1":-0.2653322251249961,
+                "2":-0.015307829790315669,
+                "3":0.16031886623305236,
+                "4":-0.17324644602384698,
+                "5":-0.2313851098218338,
+                "6":0.32800273412543784,
+                "7":-0.02829250208285845,
+                "8":-0.029796504442146224,
+                "9":-0.09624497169753406,
+                "10":-0.14203333942141338,
+                "11":0.13346486244544334,
+                "12":-0.07300195145118349,
+                "13":0.032159527658345524,
+                "14":-0.44566258359836214,
+                "15":-0.05417868981875934,
+                "16":-0.030150343272387907,
+                "17":-0.22363720331452142,
+                "18":0.12532496545456903}
+            }
+        ],
+        "biases":{
+            "sx":1,
+            "sy":1,
+            "depth":1,
+            "w":{"0":0.1}
+        }
+    },
+    {
+        "out_depth":1,
+        "out_sx":1,
+        "out_sy":1,
+        "layer_type":"relu"
+    },
+    {
+        "out_depth":5,
+        "out_sx":1,
+        "out_sy":1,
+        "layer_type":"fc",
+        "num_inputs":1,
+        "l1_decay_mul":0,
+        "l2_decay_mul":1,
+        "filters":[{
+            "sx":1,
+            "sy":1,
+            "depth":1,
+            "w":{
+                "0":-0.8326883805490437
+            }
+        },
+        {
+            "sx":1,
+            "sy":1,
+            "depth":1,
+            "w":{
+                "0":-1.97764739660907
+            }
+        },
+        {
+            "sx":1,
+            "sy":1,
+            "depth":1,
+            "w":{
+                "0":0.943349904683505
+            }
+        },
+        {
+            "sx":1,
+            "sy":1,
+            "depth":1,
+            "w":{
+                "0":-0.9824216301888558
+            }
+        },
+        {
+            "sx":1,
+            "sy":1,
+            "depth":1,
+            "w":{
+                "0":-1.493710164013261
+            }
+        }],
+        "biases":{
+            "sx":1,
+            "sy":1,
+            "depth":5,
+            "w":{
+                "0":0,
+                "1":0,
+                "2":0,
+                "3":0,
+                "4":0
+            }
+        }
+    },
+    {
+        "out_depth":5,
+        "out_sx":1,
+        "out_sy":1,
+        "layer_type":"regression",
+        "num_inputs":5
+    }]});
 }
